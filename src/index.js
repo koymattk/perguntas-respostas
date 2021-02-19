adc92cebb0b2e9ffe36cd325d516066d2ddf1703
Gabriel <koymattk@gmail.com>
2021-02-19 12:35:42 -0300

    adicionando envio da pergunta via POST

--- src/index.js
+++ src/index.js
@@ -1,19 +1,27 @@
-const express = require("express")
+const express = require("express");
 
-const app = express()
-app.set('views', './src/views')
-app.set('view engine', 'ejs')
-app.use(express.static('./src/views/public'))
+const app = express();
 
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+app.use(express.static('./src/views/public'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
 app.get('/', (req,res)=>{
     res.render('index')
-})
+});
 
 app.get('/perguntar', (req,res)=> {
-    res.render('perguntar')
-})
+    res.render('perguntar');
+});
+
+app.post('/salvarpergunta', (req,res) => {
+    const {title, description} = req.body;
+    console.log(title, description);
+    res.send('formulario recebido');
+});
 
 app.listen(5000, ()=> {
-    console.log("running on port: 5000")
-})
+    console.log("running on port: 5000");
+});
