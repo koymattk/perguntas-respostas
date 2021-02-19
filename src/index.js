d051af53fe81e2d4c3e4afc0b889c438c647f1f8
Gabriel <koymattk@gmail.com>
2021-02-19 01:40:05 -0300

    adicionando pagina de perguntas

--- src/index.js
+++ src/index.js
@@ -4,8 +4,14 @@ const app = express()
 app.set('views', './src/views')
 app.set('view engine', 'ejs')
 app.use(express.static('./src/views/public'))
+
+
 app.get('/', (req,res)=>{
     res.render('index')
+})
+
+app.get('/perguntar', (req,res)=> {
+    res.render('perguntar')
 })
 
 app.listen(5000, ()=> {
